3212abdc77b0a33e0c0c5df3a2176576f9dc9c4f
Kit <kittsville@googlemail.com>
2018-03-30 20:06:42 +0100

    Refactor CSON tests
    Reduce repeated wording

--- src/test/scala/CSONIngestionSpec.scala
+++ src/test/scala/CSONIngestionSpec.scala
@@ -5,41 +5,43 @@ import org.specs2._
 
 class CSONIngestionSpec extends mutable.Specification {
   "CSON to Recipe object ingestion specification where" >> {
-    "a Recipe must have a name" >> {
-      Fixtures.recipe.name must_== "Lentil Lasagne"
-    }
+    "a valid Recipe" >> {
+      "must have a name" >> {
+        Fixtures.recipe.name must_== "Lentil Lasagne"
+      }
 
-    "a Recipe must have a URL" >> {
-      Fixtures.recipe.url must_== "lentil-lasagne"
-    }
+      "must have a URL" >> {
+        Fixtures.recipe.url must_== "lentil-lasagne"
+      }
 
-    "a Recipe can have a source" >> {
-      Fixtures.recipe.source must beSome("https://example.com/recipes/lentil-lasagne")
-    }
+      "can have a source" >> {
+        Fixtures.recipe.source must beSome("https://example.com/recipes/lentil-lasagne")
+      }
 
-    "a Recipe need not have a source" >> {
-      Fixtures.noOptionalsRecipe.source must beNone
-    }
+      "need not have a source" >> {
+        Fixtures.noOptionalsRecipe.source must beNone
+      }
 
-    "a Recipe can have notes" >> {
-      Fixtures.recipe.notes must beSome("Consider serving with cherry tomatoes.")
-    }
+      "can have notes" >> {
+        Fixtures.recipe.notes must beSome("Consider serving with cherry tomatoes.")
+      }
 
-    "a Recipe need not have notes" >> {
-      Fixtures.noOptionalsRecipe.notes must beNone
-    }
+      "need not have notes" >> {
+        Fixtures.noOptionalsRecipe.notes must beNone
+      }
 
-    "a Recipe must have ingredients" >> {
-      Fixtures.recipe.ingredients.length must_== 2
-      Fixtures.recipe.ingredients must contain("100g Lasagne Sheets")
-      Fixtures.recipe.ingredients must contain("200g Lentils")
-    }
+      "must have ingredients" >> {
+        Fixtures.recipe.ingredients.length must_== 2
+        Fixtures.recipe.ingredients must contain("100g Lasagne Sheets")
+        Fixtures.recipe.ingredients must contain("200g Lentils")
+      }
 
-    "a recipe must have a method" >> {
-      Fixtures.recipe.method.length must_== 3
-      Fixtures.recipe.method must contain("Cook the lentils")
-      Fixtures.recipe.method must contain("Spread the lentils between lasagne sheets")
-      Fixtures.recipe.method must contain("Cook in the oven at 200C")
+      "must have a method" >> {
+        Fixtures.recipe.method.length must_== 3
+        Fixtures.recipe.method must contain("Cook the lentils")
+        Fixtures.recipe.method must contain("Spread the lentils between lasagne sheets")
+        Fixtures.recipe.method must contain("Cook in the oven at 200C")
+      }
     }
   }
 }
