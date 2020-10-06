ebd368779edd7d021357f97d7c9a2d603ddc4fc3
Kit Maywood <kit.maywood@ovoenergy.com>
2020-10-06 09:43:17 +0100

    Fix Scala formatting

--- src/test/scala/com/herokuapp/recibase/RecipesSpec.scala
+++ src/test/scala/com/herokuapp/recibase/RecipesSpec.scala
@@ -9,71 +9,89 @@ import org.http4s.circe._
 import org.http4s.implicits._
 
 class RecipesSpec extends org.specs2.mutable.Specification {
-    "list" >> {
-      "return 200" >> {
-        recipesQuery.status must beEqualTo(Status.Ok)
+  "list" >> {
+    "return 200" >> {
+      recipesQuery.status must beEqualTo(Status.Ok)
+    }
+
+    "return a link to each recipe" >> {
+      recipesQuery.as[String].unsafeRunSync() must contain(
+        "{\"name\":\"Vegetable Primavera\",\"url\":\"vegetable-primavera\"}"
+      )
+    }
+  }
+
+  "filtered list" >> {
+    "returns 200" >> {
+      filteredRecipesQuery.status must beEqualTo(Status.Ok)
+    }
+
+    "returns a link to an ingredient containing thyme" >> {
+      filteredRecipesQuery.as[String].unsafeRunSync() must contain(
+        "beetroot-risotto"
+      )
+    }
+
+    "doesn't return a link to a recipe without thyme" >> {
+      filteredRecipesQuery
+        .as[String]
+        .unsafeRunSync() must not contain ("baked-rigatoni-aubergine")
+    }
+  }
+
+  "single recipe" >> {
+    "if recipe doesn't exist" >> {
+      lazy val request = recipeQuery("i-do-not-exist")
+
+      "returns 404" >> {
+        request.status must beEqualTo(Status.NotFound)
       }
 
-      "return a link to each recipe" >> {
-        recipesQuery.as[String].unsafeRunSync() must contain("{\"name\":\"Vegetable Primavera\",\"url\":\"vegetable-primavera\"}")
+      "returns a useful error message" >> {
+        request.as[String].unsafeRunSync() must beEqualTo("Recipe not found")
       }
     }
 
-    "filtered list" >> {
+    "if a recipe exists" >> {
+      lazy val request = recipeQuery("vegetable-primavera")
+
       "returns 200" >> {
-        filteredRecipesQuery.status must beEqualTo(Status.Ok)
+        request.status must beEqualTo(Status.Ok)
       }
 
-      "returns a link to an ingredient containing thyme" >> {
-        filteredRecipesQuery.as[String].unsafeRunSync() must contain("beetroot-risotto")
-      }
-
-      "doesn't return a link to a recipe without thyme" >> {
-        filteredRecipesQuery.as[String].unsafeRunSync() must not contain("baked-rigatoni-aubergine")
+      "returns the Recipe as JSON" >> {
+        request.as[Json].unsafeRunSync() must beEqualTo(
+          VegetablePrimavera.recipe.asJson
+        )
       }
     }
-
-    "single recipe" >> {
-      "if recipe doesn't exist" >> {
-        lazy val request = recipeQuery("i-do-not-exist")
-
-        "returns 404" >> {
-          request.status must beEqualTo(Status.NotFound)
-        }
-
-        "returns a useful error message" >> {
-          request.as[String].unsafeRunSync() must beEqualTo("Recipe not found")
-        }
-      }
-
-      "if a recipe exists" >> {
-        lazy val request = recipeQuery("vegetable-primavera")
-
-        "returns 200" >> {
-          request.status must beEqualTo(Status.Ok)
-        }
-
-        "returns the Recipe as JSON" >> {
-          request.as[Json].unsafeRunSync() must beEqualTo(VegetablePrimavera.recipe.asJson)
-        }
-      }
-    }
+  }
 
   private def recipeQuery(url: String): Response[IO] = {
-    val getRecipe = Request[IO](Method.GET, Uri.unsafeFromString(s"/recipes/$url"))
+    val getRecipe =
+      Request[IO](Method.GET, Uri.unsafeFromString(s"/recipes/$url"))
     val recipeController = RecipeController.impl[IO]
-    RecibaseRoutes.recipeRoutes(recipeController).orNotFound(getRecipe).unsafeRunSync()
+    RecibaseRoutes
+      .recipeRoutes(recipeController)
+      .orNotFound(getRecipe)
+      .unsafeRunSync()
   }
 
   private lazy val recipesQuery: Response[IO] = {
     val getRecipes = Request[IO](Method.GET, uri"/recipes/")
     val recipeController = RecipeController.impl[IO]
-    RecibaseRoutes.recipeRoutes(recipeController).orNotFound(getRecipes).unsafeRunSync()
+    RecibaseRoutes
+      .recipeRoutes(recipeController)
+      .orNotFound(getRecipes)
+      .unsafeRunSync()
   }
 
   private lazy val filteredRecipesQuery: Response[IO] = {
     val getRecipes = Request[IO](Method.GET, uri"/recipes/?hasIngredient=Thyme")
     val recipeController = RecipeController.impl[IO]
-    RecibaseRoutes.recipeRoutes(recipeController).orNotFound(getRecipes).unsafeRunSync()
+    RecibaseRoutes
+      .recipeRoutes(recipeController)
+      .orNotFound(getRecipes)
+      .unsafeRunSync()
   }
 }
