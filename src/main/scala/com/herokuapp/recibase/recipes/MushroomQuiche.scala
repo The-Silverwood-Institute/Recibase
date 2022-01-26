e1c2aa985648a74683adc5a71d8f0b6aba314de3
Kit <kittsville@googlemail.com>
2022-01-26 17:36:46 +0000

    Add missing mushroom quiche ingredients

--- src/main/scala/com/herokuapp/recibase/recipes/MushroomQuiche.scala
+++ src/main/scala/com/herokuapp/recibase/recipes/MushroomQuiche.scala
@@ -24,14 +24,16 @@ object MushroomQuiche {
       Ingredient("Parmesan", None, Some("Grated")),
       Ingredient("Mascarpone", "2 tbsp"),
       Ingredient("Eggs", "2"),
+      Ingredient("Salt"),
       Ingredient("Black pepper"),
+      Ingredient("Wholegrain Mustard", "2 tsp"),
       Ingredient("Shortcrust pastry sheet", "230g")
     ),
     List(
       "Place the dried mushrooms in a small amount of boiling water to rehydrate.",
       "Add some oil to a wide pan and soften the onion.",
       "Add the mushrooms to the pan and cook until soft.",
-      "Add the mascarpone, eggs and black pepper to a mixing bowl and beat together.",
+      "Add the mascarpone, eggs, mustard, salt and black pepper to a mixing bowl and beat together.",
       "Add the mushroom mixture and dried mushrooms to the mixing bowl, including the water used to rehydrate the mushrooms if desired.",
       "Place the pastry in a 9 inch flan dish, rerolling it if necessary.",
       "Pour the mixture into the dish and top with the grated parmesan.",
