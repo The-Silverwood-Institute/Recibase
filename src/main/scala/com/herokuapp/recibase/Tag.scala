b1decf5092369c6f0aea518c119445fb9ccc9a73
Kit <kittsville@googlemail.com>
2022-03-02 11:07:14 +0000

    Group tags

--- src/main/scala/com/herokuapp/recibase/Tag.scala
+++ src/main/scala/com/herokuapp/recibase/Tag.scala
@@ -7,26 +7,34 @@ sealed abstract class Tag(override val entryName: String) extends EnumEntry
 object Tag extends Enum[Tag] with CirceEnum[Tag] {
   val values: IndexedSeq[Tag] = findValues
 
+  // Meal types
   case object Christmas extends Tag("Christmas")
   case object Pudding extends Tag("Pudding")
   case object Lunch extends Tag("Lunch")
   case object Baking extends Tag("Baking")
-
-  case object Vegetarian extends Tag("Vegetarian")
+  case object NonMeal extends Tag("Not a Meal")
   case object Soup extends Tag("Soup")
-  case object Scales extends Tag("Scales")
-  case object Freezes extends Tag("Freezes")
-  case object BetterNextDay extends Tag("Better Next Day")
-  case object Slow extends Tag("Slow")
-  case object ColdWeather extends Tag("Cold Weather")
+
+  // Dietary
   case object Vegan extends Tag("Vegan")
   case object VeganIsh extends Tag("Vegan-ish")
-  case object Spicy extends Tag("Spicy")
-  case object Stodge extends Tag("Stodge")
-  case object Quick extends Tag("Quick")
+  case object Vegetarian extends Tag("Vegetarian")
   case object VegetarianIsh extends Tag("Vegetarian-ish")
-  case object HotWeather extends Tag("Hot Weather")
-  case object HighEffort extends Tag("High Effort")
   case object Pescatarian extends Tag("Pescatarian")
-  case object NonMeal extends Tag("Not a Meal")
+
+  // Vibe
+  case object ColdWeather extends Tag("Cold Weather")
+  case object HotWeather extends Tag("Hot Weather")
+  case object Stodge extends Tag("Stodge")
+  case object Spicy extends Tag("Spicy")
+
+  // Making
+  case object Slow extends Tag("Slow")
+  case object Quick extends Tag("Quick")
+  case object Scales extends Tag("Scales")
+  case object HighEffort extends Tag("High Effort")
+
+  // Good to know
+  case object Freezes extends Tag("Freezes")
+  case object BetterNextDay extends Tag("Better Next Day")
 }
