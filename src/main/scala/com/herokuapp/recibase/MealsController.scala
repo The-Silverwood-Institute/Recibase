5386fc87813bb9247f8b58f5019c73d92ea6da4a
Kit <kittsville@googlemail.com>
2022-03-02 11:16:55 +0000

    Refactor

--- src/main/scala/com/herokuapp/recibase/MealsController.scala
+++ src/main/scala/com/herokuapp/recibase/MealsController.scala
@@ -5,11 +5,13 @@ import cats.implicits._
 
 trait MealsController[F[_]] {
   def meals: F[Set[MealStub]]
+  def mealNames: F[String]
 }
 
 object MealsController {
   def impl[F[_]: Applicative]: MealsController[F] =
     new MealsController[F] {
       override def meals: F[Set[MealStub]] = Meal.meals.pure[F]
+      override def mealNames: F[String] = Meal.meals.map(_.name).mkString("\n").pure[F]
     }
 }
