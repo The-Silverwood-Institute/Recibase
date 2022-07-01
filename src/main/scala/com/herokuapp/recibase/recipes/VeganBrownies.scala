6dab81d1b80249f484489e4348d8891eb505922b
Kit <kittsville@googlemail.com>
2022-07-01 17:10:32 +0100

    Update VeganBrownies.scala

--- src/main/scala/com/herokuapp/recibase/recipes/VeganBrownies.scala
+++ src/main/scala/com/herokuapp/recibase/recipes/VeganBrownies.scala
@@ -6,7 +6,7 @@ case object VeganBrownies extends Recipe {
   val name = "Vegan Brownies (WIP)"
 
   override val source: Option[String] = Some(
-    "https://www.louisegorrod.com/buttercup-archive/2011/07/chocolate-brownies.html"
+    "https://reciba.se/brownies"
   )
   override val description: Option[String] = Some(
     "Version 4 of my vegan alt recipe, based on Linda McCartney's classic brownies"
@@ -14,9 +14,11 @@ case object VeganBrownies extends Recipe {
   override val notes: Option[String] = Some(
     """This recipe is all about the chocolate, so use high quality 70%+ dark cooking chocolate. I use Willie's Cacao Chocolate Drops but other vegan cooking chocolates are available. Always check the ingredients as some contain milk.
         |
-        |If your vegan butter is salt free then use a full teaspoon of salt. I reduced the quantity to 1/2 tsp because Vitalite is salty. The vegan version uses way less butter, but it's still failing keep its fluffy texture, so I might reduce the vitalite further.
+        |If your vegan butter is unsalted then use a full teaspoon of salt. I reduced the quantity to 1/2 tsp because Vitalite is salty. The vegan version uses way less butter, but is still failing keep its fluffy texture, so I might reduce the vitalite further.
         |
-        |I've successfully made a gluten free (but non-vegan) version using potato flour and the taste is indistinguishable. The vegan gf versions failed to rise, but that was using a much older version of the recipe. I'll try again with gf flour once I've perfected the vegan recipe.""".stripMargin
+        |I've successfully made a gluten free (but non-vegan) version using potato flour and the taste is indistinguishable. The vegan gf versions failed to set, but that was using a much older version of the recipe. I'll try again with gf flour once I've perfected the vegan recipe.
+        |
+        |If you come up with any improvements please <a href="mailto:kittsville@gmail.com">email me</a> or DM me on Twitter: <a href="https://twitter.com/kittsville">@kittsville</a>.""".stripMargin
   )
   override val tags = Set(Tag.Pudding, Tag.Baking)
   override val image: Option[Image] = Some(
@@ -26,7 +28,12 @@ case object VeganBrownies extends Recipe {
   val ingredients = List(
     Ingredient("Vitalite", "200g"),
     Ingredient("Dark Chocolate", Some("300g"), Some("Broken into pieces")),
-    Ingredient("Aquafaba", Some("214ml"), None, Some("Collect the water from 2 tins of chickpeas")),
+    Ingredient(
+      "Aquafaba",
+      Some("214ml"),
+      None,
+      Some("Collect the water from 2 tins of chickpeas")
+    ),
     Ingredient("Cream of tartar", "1/4 tsp"),
     Ingredient("Granulated Sugar", "450g"),
     Ingredient("Vanilla Extract", "1 tbsp"),
