8451ecfc385a1c07e6292139a9c7cef96e70064c
Kit <kit@tray.io>
2023-09-07 17:13:41 +0100

    Add decoration to birthday cake recipe

--- src/main/scala/com/herokuapp/recibase/recipes/BirthdayCake.scala
+++ src/main/scala/com/herokuapp/recibase/recipes/BirthdayCake.scala
@@ -24,16 +24,39 @@ case object BirthdayCake extends Recipe {
     Ingredient("Caster Sugar", "5oz"),
     Ingredient("Eggs", "3"),
     Ingredient("Self raising flour", "8oz"),
-    Ingredient("Lemon essence", "1/4 teaspoon")
+    Ingredient("Lemon essence", "1/4 tsp"),
+    Ingredient("Unsalted Butter", "100g"),
+    Ingredient("Icing Sugar", "200g"),
+    Ingredient("Milk", "1 tbsp"),
+    Ingredient("Smarties", "136g"),
+    Ingredient(
+      "Vanilla Extract",
+      Some("1 tsp"),
+      None,
+      Some("buy vanilla extract, not vanilla flavouring")
+    ),
+    Ingredient(
+      "Marzipan",
+      None,
+      None,
+      Some("optional, if you want to decorate with lettering")
+    )
   )
 
   override val method: List[String] = List(
     "Pre-heat the oven to 165-180 C (325-350 F, gas mark 3-4).",
-    "Cream the butter and sugar.",
+    "Cream the salted butter and sugar.",
     "Lightly whisk the eggs and add to the mixture.",
     "Add the lemon essence.",
     "Fold in the flour.",
     """Put the mixture in a deep 6" or 7" cake tin.""",
-    "Bake in the oven for 1 1/4 hours."
+    "Bake in the oven for 1 1/4 hours.",
+    "Remove from the oven, leave to cool.",
+    "Optional: Slice horizontally in two, to allow for an icing filling.",
+    "For the icing: beat the unsalted butter and sugar together until smooth.",
+    "Beat in the milk and vanilla extract.",
+    "Carefully spread it over the cake and in the middle of the two halves (if applicable).",
+    "Decorate with smarties.",
+    "Optional: Roll out the marzipan, create message with letter cutters and add to top of cake."
   )
 }
