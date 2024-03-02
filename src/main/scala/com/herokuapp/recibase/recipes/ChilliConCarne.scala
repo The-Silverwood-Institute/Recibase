cd2c1be2b03f0c4a475e2d0fdf14f0548caac5ec
Kit <kittsville@googlemail.com>
2024-03-02 17:38:45 +0000

    Add missing chilli ingredients

--- src/main/scala/com/herokuapp/recibase/recipes/ChilliConCarne.scala
+++ src/main/scala/com/herokuapp/recibase/recipes/ChilliConCarne.scala
@@ -22,9 +22,11 @@ case object ChilliConCarne extends Recipe {
     Ingredient("Pepper"),
     Ingredient("Chilli Powder", "1 tsp"),
     Ingredient("Crushed or whole dried chillies", "1 tsp"),
+    Ingredient("Red Pepper", "1"),
+    Ingredient("Frozen Sweetcorn"),
     Ingredient("Paprika"),
     Ingredient("Tomato Puree"),
-    Ingredient("Tinned tomatoes"),
+    Ingredient("Peeled plum tomatoes", "1 400g tin"),
     Ingredient("Italian Herbs"),
     Ingredient("Brown Sauce"),
     Ingredient("Beef stock cube or Bovril"),
@@ -32,18 +34,21 @@ case object ChilliConCarne extends Recipe {
     Ingredient("Olive Oil"),
     Ingredient("Onion", "1"),
     Ingredient("Garlic Clove", "1"),
-    Ingredient("Tinned Kidney Beans"),
+    Ingredient("Kidney Beans", "1 400g tin"),
     Ingredient("Honey", Some("1 tbsp"), None, Some("Optional")),
+    Ingredient("Dark cooking chocolate", None, None, Some("Optional")),
     Ingredient("Mince", "500g")
   )
   val method = List(
     "Slice onions, crush garlic then brown in 2 tsp olive oil.",
+    "Add the pepper and cook for a minute.",
     "Add the mince in batches and brown then stir in chilli powder and paprika.",
     "Add the stock cube/Bovril dissolved in a small amount of water and stir.",
     "Then add 2 inches or so of tomato purée and stir again, followed by the tinned tomatoes.",
     "Simmer for half an hour then check for taste.",
     "Add chilli powder or tomato purée depending on flavour, also salt/pepper if needed.",
-    "A tablespoon of honey can improve the flavour.",
+    "Stir in the sweetcorn.",
+    "A tablespoon of honey or a few pieces of dark chocolate can improve the flavour.",
     "Serve with rice (preferably) or pasta."
   )
 }
