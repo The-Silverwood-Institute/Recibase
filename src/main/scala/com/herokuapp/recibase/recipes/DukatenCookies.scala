1901c9156df78351cd9524f5fd6e09e44ed72632
Kit <kittsville@googlemail.com>
2024-12-24 17:31:06 +0000

    Fixup dukaten recipe

--- src/main/scala/com/herokuapp/recibase/recipes/DukatenCookies.scala
+++ src/main/scala/com/herokuapp/recibase/recipes/DukatenCookies.scala
@@ -34,7 +34,7 @@ case object DukatenCookies extends Recipe {
 
   val ingredientsBlocks = List(
     IngredientsBlock(
-      "Buscuits".some,
+      "Biscuits".some,
       List(
         Ingredient("Flour", "250g"),
         Ingredient("Baking Powder", "1tsp"),
@@ -55,12 +55,7 @@ case object DukatenCookies extends Recipe {
       List(
         Ingredient("Butter", "125g"),
         Ingredient("Icing Sugar", "130g"),
-        Ingredient(
-          "Cocoa",
-          "1 spoon".some,
-          None,
-          "use a large serving spoon".some
-        ),
+        Ingredient("Cocoa", "1 heaped tablespoon"),
         Ingredient("Rum flavouring", "1 top"),
         Ingredient("Egg", "1")
       )
@@ -69,14 +64,9 @@ case object DukatenCookies extends Recipe {
       "Frosting".some,
       List(
         Ingredient("Powdered Sugar", "100g"),
-        Ingredient(
-          "Cocoa",
-          "1 spoon".some,
-          None,
-          "use a large serving spoon".some
-        ),
+        Ingredient("Cocoa", "1 heaped tablespoon"),
         Ingredient("Water", "1-2 tbsp".some, "hot".some, None),
-        Ingredient("Butter", "1 tbsp".some, "melted".some)
+        Ingredient("Butter", "28g".some, "melted".some)
       )
     )
   )
@@ -84,17 +74,17 @@ case object DukatenCookies extends Recipe {
   override val method: List[String] = List(
     s"Pre-heat the oven to ${190.celsius}",
     "Rub a large baking tray with butter or margarine.",
-    "Combine all the buscuit ingredients in order.",
+    "Combine all the biscuit ingredients in order.",
     "Mix together then knead until smooth. If the mixture is too soft then put it in the fridge or freezer for a moment.",
     "Roll out thin and cut into 4cm circles.",
     "Lay out on the baking tray and bake for 10 minutes or until a light golden colour.",
     "Put to one side to cool.",
-    "While the buscuits are baking you can also make the filling and frosting:",
+    "While the biscuits are baking you can also make the filling and frosting:",
     "Make the filling by combining all the ingredients with a food mixer.",
     "Make the frosting by mixing all the ingredients until smooth",
-    "Once the buscuits are cool lay out half of them on a sheet of baking paper. Leave room between the buscuits and the edge for the frosting to ooze.",
-    "Place a drop of the filling on top of each buscuit then flatten the mixture by placing down another buscuit on top.",
-    "Once all the buscuits are filled drizzle the frosting over one side of each biscuit.",
-    "Transfer the buscuits to the fridge to set the frosting and filling."
+    "Once the biscuits are cool lay out half of them on a sheet of baking paper. Leave room between the biscuits and the edge for the frosting to ooze.",
+    "Place a drop of the filling on top of each biscuit then flatten the mixture by placing down another biscuit on top.",
+    "Once all the biscuits are filled drizzle the frosting over one side of each biscuit.",
+    "Transfer the biscuits to the fridge to set the frosting and filling."
   )
 }
