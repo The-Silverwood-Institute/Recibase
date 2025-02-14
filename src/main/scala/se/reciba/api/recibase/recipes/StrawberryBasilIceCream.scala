eb9559d50c58907714d3291fb2aa80b96e0d21b8
Kit <kittsville@googlemail.com>
2025-02-14 23:28:13 +0000

    Add more basil ice cream notes

--- src/main/scala/se/reciba/api/recibase/recipes/StrawberryBasilIceCream.scala
+++ src/main/scala/se/reciba/api/recibase/recipes/StrawberryBasilIceCream.scala
@@ -30,7 +30,7 @@ case object StrawberryBasilIceCream extends Recipe with IceCream {
         |
         |Stephani tasting notes: Interesting and complex. Most liked so far. Strong basil flavour with moments of strawberry and black pepper.
         |
-        |Kit tasting notes: smells like pesto, tastes juicy and fruity with strawberry dominating the basil. Pepper a bit weak. Might improve after a few days in the freezer but I should consider soaking the pepper in the mascarpone for a day in the fridge before making ice cream.""".stripMargin
+        |Kit tasting notes: smells like pesto, tastes juicy and fruity with strawberry dominating the basil. Pepper a bit weak. Might improve after a few days in the freezer but I should consider soaking the pepper in the mascarpone for a day in the fridge before making ice cream. Consider adding a dash of lime juice to balance the strawberry sweetness, like <a href="https://peterleymanorfarm.co.uk/strawberry-basil-and-black-pepper-ice-cream/">this recipe</a> does.""".stripMargin
   )
   override val tags = Set(Tag.Pudding)
   override val image: Option[Image] = Some(
