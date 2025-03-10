92b18e331493a901e1d5150aa9c3db9d79b82207
Kit <kittsville@googlemail.com>
2025-03-10 22:16:09 +0000

    My kingdom for a proofreader

--- src/main/scala/se/reciba/api/recibase/recipes/PomegranateLimeIceCream.scala
+++ src/main/scala/se/reciba/api/recibase/recipes/PomegranateLimeIceCream.scala
@@ -11,28 +11,28 @@ import se.reciba.api.model.{
   Permalink
 }
 
-case object PomegraniteLimeIceCream extends Recipe with IceCream {
-  val name = "Pomegranite & Lime Ice Cream"
+case object PomegranateLimeIceCream extends Recipe with IceCream {
+  val name = "Pomegranate & Lime Ice Cream"
 
   override val permalink: Permalink =
-    Permalink("pomegranite-lime-ice-cream")
+    Permalink("pomegranate-lime-ice-cream")
 
   override val source: Option[String] = Some("Kit")
 
   override val notes: Option[String] = Some(
     s"""$genericNotes
         |
-        |Kit tasting notes: A perfectly enjoyable but fairly tame flavour profile. At least compared to the weirder flavours I've made. I'm glad I added as much lime as I did because it provides a necessary punchy comparison to the subtler pomegranite. I should have used fresh lime juice and zest but I think I went shopping in a bit of a rush.""".stripMargin
+        |Kit tasting notes: A perfectly enjoyable but fairly tame flavour profile. At least compared to the weirder flavours I've made. I'm glad I added as much lime as I did because it provides a necessary punchy comparison to the subtler pomegranate. I should have used fresh lime juice and zest but I think I went shopping in a bit of a rush.""".stripMargin
   )
   val tags = Set(Tag.Pudding)
   override val image: Option[Image] = Some(
-    Image("https://i.reciba.se/pomegranite-lime-ice-cream.jpg")
+    Image("https://i.reciba.se/pomegranate-lime-ice-cream.jpg")
   )
 
   val ingredientsBlocks = List(
     genericIngredients.prefixIngredients(
       Ingredient(
-        "Pomegranite powder",
+        "Pomegranate powder",
         "40g".some,
         None,
         "freeze dried, no added sugar".some
@@ -42,7 +42,7 @@ case object PomegraniteLimeIceCream extends Recipe with IceCream {
   )
 
   val method = genericMethodStart ++ List(
-    "Add the mascarpone cheese, pomegranite powder and lime juice. Gently stir in the powder to avoid it going everywhere.",
+    "Add the mascarpone cheese, pomegranate powder and lime juice. Gently stir in the powder to avoid it going everywhere.",
     "Continue whisking until thoroughly mixed."
   ) ++ genericMethodEnd
 }
