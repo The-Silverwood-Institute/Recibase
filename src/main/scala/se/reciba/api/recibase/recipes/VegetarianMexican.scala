41c9144fd5cb264108a35c7a404da9f8999907f0
Kit <kittsville@googlemail.com>
2025-08-25 20:55:46 +0100

    Scalafmt

--- src/main/scala/se/reciba/api/recibase/recipes/VegetarianMexican.scala
+++ src/main/scala/se/reciba/api/recibase/recipes/VegetarianMexican.scala
@@ -34,7 +34,7 @@ case object VegetarianMexican extends Recipe {
     Ingredient("Cinnamon"),
     Ingredient("Cumin"),
     Ingredient("Salt"),
-    Ingredient("Soured Cream", None, None, Some("optional")),
+    Ingredient("Soured Cream", None, None, Some("optional"))
   )
   val method = List(
     s"Preheat the oven to ${200.celsius}.",
